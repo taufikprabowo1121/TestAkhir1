bfb0e946ecec466a46957d9f13bd3a017a912f8a
taufikprabowo1121 <solidiercyber@gmail.com>
2017-08-23 17:02:39 +0700

    adad

---
+++ Soal2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soal2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int diameter;
+            double phi = 3.14;
+           
+            Console.Write("Masukan diameter : ");
+            diameter = int.Parse(Console.ReadLine());
+            double radius = diameter / 2;
+            double luas = phi * radius * radius;
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Radius : " + radius);
+            Console.WriteLine("Luas dan Keliling Lingkaran :"+ luas );
+            Console.WriteLine("---------------------------");
+
+            Console.ReadKey();
+        }
+    }
+}
